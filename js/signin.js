a7b5e96ff8d9eb5363a23ca0874dd38c85118374
mohamad-tahan <mohamadtahan880@gmail.com>
2022-06-08 12:34:39 +0300

    Added Sign in function

---
+++ js/signin.js
@@ -0,0 +1,36 @@
+let type = 0;
+function signin() {
+  let login = document.getElementById("s");
+
+  /*When the user fills the values and submits, send the values to the url to save them in the db*/
+  login.addEventListener("click", function (e) {
+    e.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let data = {
+      email: email,
+      password: password,
+    };
+
+    fetch("http://127.0.0.1:8000/api/signIn", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.type == 0) {
+          location.href = "./signup.html";
+          console.log(json.type);
+          console.log(json);
+        } else {
+          location.href = "";
+        }
+      })
+      .catch((err) => console.log(err));
+
+    
+  });
+}
+signin();
